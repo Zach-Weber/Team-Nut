643e86dd7a7403a305b34f01866fd24719a4781f
Conor Brennan <conor.brennan@digipen.edu>
2020-01-30 16:35:13 -0800

    Added Assets to Git

---
+++ Unity Project/Dino Game/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody2D myRB;
+    public float speed = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRB = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 newVel = myRB.velocity;
+        newVel.x = speed;
+        myRB.velocity = newVel;
+
+    }
+}
