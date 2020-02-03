8e3428837b5652459fbe90697dbe329a9e778a54
Conor Brennan <conor.brennan@digipen.edu>
2020-02-03 15:27:10 -0800

    Pushing Missing assets

---
+++ Unity Project/Dino Game/Assets/Scripts/PrototypeGenerate.cs
@@ -0,0 +1,37 @@
+﻿/******************************
+ * PrototypeGenerate.cs
+ * By: Conor Brennan
+ * Last Edited: 1/31/2020
+ * Description: simple ground generation, not random, just for prototype
+ ******************************/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrototypeGenerate : MonoBehaviour
+{
+    GameObject player;
+    public GameObject groundSection;
+    public int groundPos;
+    public int playerPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = FindObjectOfType<PlayerController>().gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+     if(player != null)
+        {
+            if(player.transform.position.x >= playerPos)
+            {
+                Instantiate(groundSection, new Vector3(groundPos, 0, 0), Quaternion.identity);
+                groundPos += 30;
+                playerPos += 30;
+            }
+        }   
+    }
+}
