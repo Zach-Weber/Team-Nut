45afc3ba7589d8169c0f1d5c341b50d66758797a
Conor Brennan <conor.brennan@digipen.edu>
2020-02-03 16:42:11 -0800

    PlayerController updated

--- Unity Project/Dino Game/Assets/Scripts/PlayerController.cs
+++ Unity Project/Dino Game/Assets/Scripts/PlayerController.cs
@@ -1,7 +1,7 @@
 ﻿/******************************
  * PlayerController.cs
  * By: Conor Brennan
- * Last Edited: 1/31/2020
+ * Last Edited: 2/3/2020
  * Description: gives player forward movement at game start and controls jumping
  ******************************/
 using System.Collections;
@@ -14,6 +14,8 @@ public class PlayerController : MonoBehaviour
     Rigidbody2D myRB;
     public float speed = 5f;
     public float jumpSpeed = 1f;
+    public bool started = false;
+    public bool jumped = false;
 
     // Start is called before the first frame update
     void Start()
@@ -24,24 +26,44 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //gives player forward movement
-        Vector3 newVel = myRB.velocity;
-        newVel.x = speed;
-        myRB.velocity = newVel;  
-
-        //checks if player is on ground
-        if(transform.position.y < -1.9)
+        if(started == false)
         {
             if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Jump();
+                jumped = true;
+            }
+            if (myRB.position.y != 0 && jumped == true)
+            {
+                started = true;
+                Debug.Log("Started");
+            }
+        }
+        //gives player forward movement
+        if (started == true)
+        {
+            Vector3 newVel = myRB.velocity;
+            newVel.x = speed;
+            myRB.velocity = newVel;  
+        }
+
+        //checks if player is on ground
+        if (myRB.velocity.y == 0)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+                Jump();
+        }
+                
+        void Jump()
+        {
             {
                 //creates new vector for jump movement
                 Vector3 jumpMovement = new Vector3(0.0f, 1.0f, 0.0f);
                 //sets player velocity to jumpmovement * jumpspeed
                 myRB.velocity = jumpMovement * jumpSpeed;
-
-                Debug.Log("Jump");
             }
         }
+        
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -50,6 +72,8 @@ public class PlayerController : MonoBehaviour
         if (collision.collider.gameObject.tag == "Obstacle")
         {
             Destroy(gameObject);
+            started = false;
+            jumped = false;
         }
     }
 }
