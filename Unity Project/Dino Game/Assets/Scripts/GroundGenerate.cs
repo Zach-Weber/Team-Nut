d46088654010cb347f8a7b70d4d00120cae53825
Zach-Weber <zachary.weber@digipen.edu>
2020-02-04 17:52:37 -0800

    GroundGenerate.cs

--- Unity Project/Dino Game/Assets/Scripts/GroundGenerate.cs
+++ Unity Project/Dino Game/Assets/Scripts/GroundGenerate.cs
@@ -11,7 +11,9 @@ using UnityEngine;
 public class GroundGenerate : MonoBehaviour
 {
     GameObject player;
-    public GameObject groundSection;
+    public GameObject ground1;
+    public GameObject ground2;
+    public GameObject ground3;
     public int groundPos;
     public int playerPos;
 
@@ -24,14 +26,31 @@ public class GroundGenerate : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-     if(player != null)
+        if (player != null)
         {
-            if(player.transform.position.x >= playerPos)
+            if (player.transform.position.x >= playerPos)
             {
-                Instantiate(groundSection, new Vector3(groundPos, 0, 0), Quaternion.identity);
-                groundPos += 30;
-                playerPos += 30;
+                switch (Random.Range(1, 3))
+                {
+                    case 1: // Ground w/ no bumps
+                        Instantiate(ground1, new Vector3(groundPos, 0, 0), Quaternion.identity);
+                        groundPos += 30;
+                        playerPos += 30;
+                        break;
+
+                    case 2: // Ground w/ Double Bumps
+                        Instantiate(ground2, new Vector3(groundPos, 0, 0), Quaternion.identity);
+                        groundPos += 30;
+                        playerPos += 30;
+                        break;
+
+                    case 3: // Ground w/ Bump and Divot
+                        Instantiate(ground3, new Vector3(groundPos, 0, 0), Quaternion.identity);
+                        groundPos += 30;
+                        playerPos += 30;
+                        break;
+                }
             }
-        }   
+        }
     }
 }
