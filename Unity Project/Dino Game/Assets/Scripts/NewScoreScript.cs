b50ca829c677a048f3fcfd14196840825ce41e11
conorpb <56563669+conorpb@users.noreply.github.com>
2020-02-06 21:05:10 -0800

    Updated score script to work in game

---
+++ Unity Project/Dino Game/Assets/Scripts/NewScoreScript.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NewScoreScript : MonoBehaviour
+{
+    private static Text Score;
+    private static Text HighScore;
+    public PlayerController playerController;
+
+    public int score = 0;
+    public int highScore = 0;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        HighScore = GameObject.Find("HighScore").GetComponent<Text>();
+        Score = GameObject.Find("Score").GetComponent<Text>();
+        playerController = GameObject.Find("Player").GetComponent<PlayerController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateScore();
+        UpdateHighScore();
+    }
+
+    public void UpdateScore()
+    {
+        if (playerController.started)
+        {
+            score = +(int)GameObject.Find("Player").transform.position.x;
+            Score.text = score.ToString("D5");
+        }
+        return;
+    }
+
+    public void UpdateHighScore()
+    {
+        if (score > highScore)
+        {
+            highScore = score;
+            HighScore.text = highScore.ToString("D5");
+        }
+    }
+}
+
