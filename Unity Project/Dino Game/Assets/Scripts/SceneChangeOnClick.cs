e3b78e16fa150e971791520ad6469be2106c2eff
Zach-Weber <zachary.weber@digipen.edu>
2020-02-10 16:14:02 -0800

    Finished Main Menu

---
+++ Unity Project/Dino Game/Assets/Scripts/SceneChangeOnClick.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChangeOnClick : MonoBehaviour
+{
+    public string NextScene;
+
+    public void SwitchScene()
+    {
+        SceneManager.LoadScene(NextScene);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
